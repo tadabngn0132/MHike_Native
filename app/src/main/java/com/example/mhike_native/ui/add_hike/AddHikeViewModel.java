f30a21f2c6d75202f56b4ed103d54e8395fb788a
Nguyen Ba Dat <datngba2310@gmail.com>
2025-11-11 11:21:58 +0700

    Refactor AddHikeViewModel to extend AndroidViewModel; implement addHike method for adding hike details to the database

--- app/src/main/java/com/example/mhike_native/ui/add_hike/AddHikeViewModel.java
+++ app/src/main/java/com/example/mhike_native/ui/add_hike/AddHikeViewModel.java
@@ -1,19 +1,31 @@
 package com.example.mhike_native.ui.add_hike;
 
+import android.app.Application;
+import android.provider.ContactsContract;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.AndroidViewModel;
 
-public class AddHikeViewModel extends ViewModel {
+import com.example.mhike_native.DatabaseHelper;
+import com.example.mhike_native.models.Hike;
 
-    private final MutableLiveData<String> mText;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public AddHikeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is add hike fragment");
+public class AddHikeViewModel extends AndroidViewModel {
+    private final DatabaseHelper databaseHelper;
+
+    public AddHikeViewModel(@NonNull Application application) {
+        super(application);
+        this.databaseHelper = new DatabaseHelper(getApplication());
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    private void addHike(String name, String location, String dateString, boolean parkingAvailable, double length, String difficulty, String description) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(dateString, formatter);
+        Hike hike = new Hike(name, location, date, parkingAvailable, length, difficulty, description);
+        databaseHelper.addHike(hike);
     }
 }
